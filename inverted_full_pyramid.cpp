dda696c95092d49ea8f7d74b8a7bad64972b2cca
hari <103772225+haristunner@users.noreply.github.com>
2022-10-16 15:04:08 +0530

    Update inverted_full_pyramid.cpp

--- inverted_full_pyramid.cpp
+++ inverted_full_pyramid.cpp
@@ -5,15 +5,17 @@ int main()
     int n;
 	cin>>n;
 	for(int i=1;i<=n;i++){
-		for(int j=n;j>=1;j--){
-		    if(i>=j)
-		    cout<<"*";
-		    else
+		for(int j=1;j<=n;j++){
+		    if(i>j)
 		    cout<<" ";
+		    else
+		    cout<<"*";
 		}
-		for(int k=1; k<=n; k++)
+		for(int k=n; k>=1; k--)
 		{
-		    if(k<i)
+		    if(k<=i)
+		    cout<<" ";
+		    else
 		    cout<<"*";
 		}
 		cout<<endl;
